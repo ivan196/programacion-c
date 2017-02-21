ce75f89e2debc6d4cda1969ce5afe97c658a9620
ivan <ivantome.ciclos@gmail.com>
2017-02-21 08:47:59 +0100

    ejercicios funciones

---
+++ 2017/12_ejercicios_funciones/tres.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void pedir(char tablero[3][3], char marca){
+	int x, y;
+
+	printf()
+}
+
+
+
+int main(int argc, char *argv[]){
+
+	
+
+
+
+	return EXIT_SUCCESS;
+}
