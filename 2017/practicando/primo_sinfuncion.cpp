85093680b4a569d35a5719c6c7f1f1ad75b4c908
ivan196 <ivancrespoc@gmail.com>
2017-03-06 07:53:03 +0100

    practicando

--- 2017/practicando/primo_sinfuncion.cpp
+++ 2017/practicando/primo_sinfuncion.cpp
@@ -9,21 +9,21 @@ int main(int argc, char *argv[]){
 	int numero;
 	int pos_primo = 0;
 
-	printf("Dime un numero: ");
+	printf("Dime tu numero: ");
 	scanf(" %i", &numero);
+	
 
 	for(int divisor=1; divisor<=numero; divisor++){
 		if(numero%divisor == 0)
 			pos_primo += 1;
+
 	}
 
 	if(pos_primo == 2)
-		printf("el numero es primo\n");
+		printf("El numero es primo\n");
 
 	if(pos_primo > 2)
-		printf("el numero no es primo\n");
-
-
+		printf("El numero no es primo\n");
 
 	return EXIT_SUCCESS;
 }
